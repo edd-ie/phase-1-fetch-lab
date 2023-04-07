2a61ad098239fea928a517b7362fa2700dad1443
_Edd.ie_ <57677657+edd-ie@users.noreply.github.com>
2023-04-07 13:14:09 +0300

    return f(x)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books").then((raw) => raw.json()).then((data) => renderBooks(data));
+  return fetch("https://anapioficeandfire.com/api/books").then(raw => raw.json()).then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
